7794ce9eba4ad136b7c04c1ea9d9848e8146ea72
Ge Jun <ge.jun@grabtaxi.com>
2016-10-15 14:50:43 +0800

    modify scripts

--- load/district.py
+++ load/district.py
@@ -3,7 +3,7 @@ import csv
 
 with open('cassandra/district.csv', 'wb') as f:  # output csv file
     writer = csv.writer(f)
-    with open('D8-data/district.csv', 'r') as districtfile:  # input csv file
+    with open('D40-data/district.csv', 'r') as districtfile:  # input csv file
         districtdict = csv.DictReader(districtfile, delimiter=',')
         for row in districtdict:
             temp = row["D_W_ID"], row["D_ID"], row["D_NEXT_O_ID"], row["D_YTD"]
