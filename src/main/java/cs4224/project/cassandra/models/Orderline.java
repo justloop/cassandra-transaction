c81ba774dc4139f373efd2f23c5d46db9fde80c5
cuixiang23@gmail.com <cuixiang23@gmail.com>
2016-10-15 12:11:38 +0800

    commit

--- src/main/java/cs4224/project/cassandra/models/Orderline.java
+++ src/main/java/cs4224/project/cassandra/models/Orderline.java
@@ -2,4 +2,25 @@ package cs4224.project.cassandra.models;
 
 public class Orderline {
 
+	private int ol_i_id;
+	private double ol_amount;
+	
+	public Orderline(int a, double b){
+		ol_i_id = a;
+		ol_amount = b;
+	}
+	
+	public int getId() {
+		return ol_i_id;
+	}
+	
+	public double getAmount() {
+		return ol_amount;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("<%d,%5.2f>", ol_i_id, ol_amount);
+	}
 }
+
